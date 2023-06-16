57a784f254033579864b59285b849a45ab3913e9
Kamil Stecyk <kamils987@interia.eu>
2023-06-16 19:34:04 +0200

    DfRobot working example

--- HRV/sketch_jun14a/sketch_jun14a.ino
+++ HRV/sketch_jun14a/sketch_jun14a.ino
@@ -1,46 +1,63 @@
-#include <Wire.h>
-#include "DFRobotFermion.h"
+/*!
+  * @file  gainHeartbeatSPO2.ino
+  * @n experiment phenomena: get the heart rate and blood oxygenation, during the update the data obtained does not change
+  * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+  * @licence     The MIT License (MIT)
+  * @author      PengKaixing(kaixing.peng@dfrobot.com)
+  * @version     V1.0
+  * @date        2021-06-21
+  * @get         from https://www.dfrobot.com
+  * @url         https://github.com/DFRobot/DFRobot_BloodOxygen_S
+*/
+#include "DFRobot_BloodOxygen_S.h"
 
-DFRobotFermion fermion;
-int heartRate;
-RunningStatistics intervalStats;
+#define I2C_COMMUNICATION  //use I2C for communication, but use the serial port for communication if the line of codes were masked
 
-#define SDA_PIN 21
-#define SCL_PIN 22
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  while (!Serial);  // Wait for serial connection
-
-  Wire.begin(SDA_PIN, SCL_PIN);  // Inicjalizacja komunikacji I2C z odpowiednimi pinami SDA i SCL
-  fermion.begin();  // Initialize the Fermion sensor
-
+#ifdef  I2C_COMMUNICATION
+#define I2C_ADDRESS    0x57
+  DFRobot_BloodOxygen_S_I2C MAX30102(&Wire ,I2C_ADDRESS);
+#else
+/* ---------------------------------------------------------------------------------------------------------------
+ *    board   |             MCU                | Leonardo/Mega2560/M0 |    UNO    | ESP8266 | ESP32 |  microbit  |
+ *     VCC    |            3.3V/5V             |        VCC           |    VCC    |   VCC   |  VCC  |     X      |
+ *     GND    |              GND               |        GND           |    GND    |   GND   |  GND  |     X      |
+ *     RX     |              TX                |     Serial1 TX1      |     5     |   5/D6  |  D2   |     X      |
+ *     TX     |              RX                |     Serial1 RX1      |     4     |   4/D7  |  D3   |     X      |
+ * ---------------------------------------------------------------------------------------------------------------*/
+#if defined(ARDUINO_AVR_UNO) || defined(ESP8266)
+SoftwareSerial mySerial(4, 5);
+DFRobot_BloodOxygen_S_SoftWareUart MAX30102(&mySerial, 9600);
+#else
+DFRobot_BloodOxygen_S_HardWareUart MAX30102(&Serial1, 9600); 
+#endif
+#endif
+void setup()
+{
+  Serial.begin(115200);
+  while (false == MAX30102.begin())
+  {
+    Serial.println("init fail!");
+    delay(1000);
+  }
+  Serial.println("init success!");
+  Serial.println("start measuring...");
+  MAX30102.sensorStartCollect();
 }
 
-void loop() {
-  // Read heart rate from the sensor
-  heartRate = fermion.getHeartRate();
-
-  // Check if a valid heart rate is obtained
-  if (heartRate > 0) {
-    Serial.print("Heart Rate: ");
-    Serial.println(heartRate);
-
-    // Calculate HRV
-    if (intervalStats.count() > 0) {
-      unsigned long currentMillis = millis();
-      unsigned long interval = currentMillis - previousMillis;
-      previousMillis = currentMillis;
-      intervalStats.push(interval);
-
-      Serial.print("HRV: ");
-      Serial.println(intervalStats.standardDeviation());
-    } else {
-      previousMillis = millis();
-    }
-  }
-
-  delay(1000);  // Delay for 1 second before reading again
-
+void loop()
+{
+  MAX30102.getHeartbeatSPO2();
+  Serial.print("SPO2 is : ");
+  Serial.print(MAX30102._sHeartbeatSPO2.SPO2);
+  Serial.println("%");
+  Serial.print("heart rate is : ");
+  Serial.print(MAX30102._sHeartbeatSPO2.Heartbeat);
+  Serial.println("Times/min");
+  Serial.print("Temperature value of the board is : ");
+  Serial.print(MAX30102.getTemperature_C());
+  Serial.println(" ℃");
+  //The sensor updates the data every 4 seconds
+  delay(4000);
+  //Serial.println("stop measuring...");
+  //MAX30102.sensorEndCollect();
 }
